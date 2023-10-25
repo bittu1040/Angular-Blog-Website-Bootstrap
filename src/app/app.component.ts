584a28672ea5eb37eeec16b240e1717a9138a849
Bittu <bittu1040@gmail.com>
2023-10-25 19:43:27 +0530

    added user inactivity feature at 60 seconds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blog-Website';
+  timeoutId:any;
+  userInactive: Subject<any> = new Subject();
+ 
+ constructor() {
+    this.checkTimeOut();
+    this.userInactive.subscribe((message) => {
+      // Logout logic or redirect to login page
+      // alert(message);
+      console.log(message)
+    });
+  }
+ 
+  checkTimeOut() {
+    this.timeoutId = setTimeout(
+      () => this.userInactive.next("User has been inactive for 60 seconds"), 60000);
+  }
+ 
+  @HostListener('window:keydown', ['$event'])
+  checkUserKeyboardActivity(event:KeyboardEvent) {
+    console.log("keyboard event", event)
+    clearTimeout(this.timeoutId);
+    this.checkTimeOut();
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  checkUserMouseActivity(event: MouseEvent) {
+    console.log("mouse event", event)
+    clearTimeout(this.timeoutId);
+    this.checkTimeOut();
+  }
+
+  @HostListener('window:click', ['$event'])
+  checkUserClickActivity(event: any) {
+    console.log("click event", event)
+    clearTimeout(this.timeoutId);
+    this.checkTimeOut();
+  }
 }
